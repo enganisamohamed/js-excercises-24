477f6c39cefb63d635dea0353180e47e35dda0d3
enganisamohamed <enganisamohamed@gmail.com>
2026-03-03 05:05:53 -0800

    adding excercise six

--- mainjs5.js
+++
@@ -1,7 +0,0 @@
-// Arrow function
-const add = (a, b) => a + b;
-
-// Test
-console.log(add(8, 12));  // Output: 20
-console.log(add(14, 16)); // Output: 30
-console.log(add(5, 5));   // Output: 10
---
+++ mainjs6.js
@@ -0,0 +1,5 @@
+let colors = ["red", "green", "blue"];
+colors.push("yellow");
+console.log(colors); 
+colors.shift();
+console.log(colors); 
