ece82c38fe79ecde686cabb417575155dff283c7
enganisamohamed <enganisamohamed@gmail.com>
2026-03-12 07:56:24 -0700

    adding exercise 20

--- mainjs19.js
+++
@@ -1,4 +0,0 @@
-const numbers = [1,2,3,4,5]
-numbers.forEach( numbers => {
-console.log (numbers)
-})
---
+++ mainjs20.js
@@ -0,0 +1,5 @@
+const numbers = ["apple", "banana", "cherlly"];
+
+const squareNumbers = numbers.map(num => num.length);
+
+console.log(squareNumbers);
