760a5fd53c4bbbb3c336145c388086bed6c9240c
enganisamohamed <enganisamohamed@gmail.com>
2026-03-17 06:15:45 -0700

    adding exercise24

--- mainjs23.js
+++
@@ -1,6 +0,0 @@
-const car = { make: "toyoto", model: "corolla", year: 2024 };
-
-let { make, model, year } = car;
-
-console.log(make);
-console.log(model);
---
+++ mainjs24.js
@@ -0,0 +1,6 @@
+function calculateArea(width, hight) {
+    return width * hight;
+}
+
+let area = calculateArea(5, 5);
+console.log("Area is:" + area);
